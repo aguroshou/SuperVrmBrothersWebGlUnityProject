5b18b36217dfd3325e8fd32d01026a05857bca54
aguroshou <aguroshou0413@gmail.com>
2020-04-18 00:11:03 +0900

    Get item but no function.

--- Assets/Aguro/Scripts/RotateItem.cs
+++ Assets/Aguro/Scripts/RotateItem.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class RotateItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0f, 0f, -100f * Time.deltaTime));
     }
 }
--- Assets/Aguro/Scripts/ShadowCollisionSetting.cs
+++ Assets/Aguro/Scripts/ShadowCollisionSetting.cs
@@ -7,23 +7,35 @@ public class ShadowCollisionSetting : MonoBehaviour
 {
     public Text CoinScoreText;
     public long CoinScore;
+    public long ObtainedStar;
+    public long ObtainedJumpItem;
+    public long ObtainedHeart;
+    public Transform RespawnPoint;
+    public GameObject PastCheckPoint;
 
     // Start is called before the first frame update
     void Start()
     {
         CoinScore = 0;
+        ObtainedStar = 0;
+        ObtainedJumpItem = 0;
+        ObtainedHeart = 0;
+        RespawnPoint = this.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         CoinScoreText.text = "SCORE" + CoinScore;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            this.transform.position = RespawnPoint.position;
+        }
     }
 
     // 他のオブジェクトと衝突した時に呼び出される関数
     private void OnTriggerEnter(Collider collision)
     {
-        
         if (collision.name.Contains("GoldCoin"))
         {
             CoinScore += 100;
@@ -36,8 +48,35 @@ public class ShadowCollisionSetting : MonoBehaviour
         }
         else if (collision.name.Contains("CopperCoin"))
         {
-            CoinScore += 1;
+            CoinScore++;
             Destroy(collision.gameObject);
+        }
+        else if (collision.name.Contains("Star"))
+        {
+            ObtainedStar++;
+            Destroy(collision.gameObject);
+        }
+        else if (collision.name.Contains("JumpItem"))
+        {
+            ObtainedJumpItem++;
+            Destroy(collision.gameObject);
+        }
+        else if (collision.name.Contains("Heart"))
+        {
+            ObtainedHeart++;
+            Destroy(collision.gameObject);
+        }
+        else if (collision.name.Contains("CheckPoint"))
+        {
+            RespawnPoint = collision.transform;
+            if (PastCheckPoint != null)
+            {
+                MeshRenderer PastCheckPointMesh = PastCheckPoint.GetComponent<MeshRenderer>();
+                PastCheckPointMesh.material.color = Color.gray;
+            }
+            PastCheckPoint = collision.gameObject;
+            MeshRenderer TmpMesh = collision.GetComponent<MeshRenderer>();
+            TmpMesh.material.color = Color.green;
         }
     }
 }

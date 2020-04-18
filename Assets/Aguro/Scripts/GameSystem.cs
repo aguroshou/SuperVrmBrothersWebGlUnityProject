3a66c0103601f9ca9da58e19575acf168a634417
aguroshou <aguroshou0413@gmail.com>
2020-04-18 21:59:24 +0900

    Add Damage cube.

--- Assets/Aguro/Scripts/GameSystem.cs
+++ Assets/Aguro/Scripts/GameSystem.cs
@@ -20,6 +20,19 @@ public class GameSystem : MonoBehaviour
     public GameObject PastCheckPoint;
     private MoveBehaviour MoveBehaviorScript;
 
+    private AudioSource CheckPointSound;
+    private AudioSource CoinCopperSound;
+    private AudioSource CoinSilverSound;
+    private AudioSource CoinGoldSound;
+    private AudioSource DamageSound;
+    private AudioSource HeartOnly1Sound;
+    private AudioSource JumpItemSound;
+    private AudioSource JumpSound;
+    private AudioSource HeartItemSound;
+    private AudioSource PlayerDeadSound;
+    private AudioSource StageClearSound;
+    private AudioSource StarItemSound;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +44,19 @@ public class GameSystem : MonoBehaviour
         RemainingHeart = 1;
         RespawnPoint = this.transform.position;
         MoveBehaviorScript = this.GetComponent<MoveBehaviour>();
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        CheckPointSound = audioSources[0];
+        CoinCopperSound = audioSources[1];
+        CoinSilverSound = audioSources[2];
+        CoinGoldSound = audioSources[3];
+        DamageSound = audioSources[4];
+        HeartOnly1Sound = audioSources[5];
+        JumpItemSound = audioSources[6];
+        JumpSound = audioSources[7];
+        HeartItemSound = audioSources[8];
+        PlayerDeadSound = audioSources[9];
+        StageClearSound = audioSources[10];
+        StarItemSound = audioSources[11];
     }
 
     // Update is called once per frame
@@ -55,31 +81,41 @@ public class GameSystem : MonoBehaviour
     {
         if (collision.name.Contains("GoldCoin"))
         {
+            CoinGoldSound.PlayOneShot(CoinGoldSound.clip);
             CoinScore += 100;
             Destroy(collision.gameObject);
         }
         else if (collision.name.Contains("SilverCoin"))
         {
+            CoinSilverSound.PlayOneShot(CoinSilverSound.clip);
             CoinScore += 10;
             Destroy(collision.gameObject);
         }
         else if (collision.name.Contains("CopperCoin"))
         {
+            CoinCopperSound.PlayOneShot(CoinCopperSound.clip);
             CoinScore++;
             Destroy(collision.gameObject);
         }
         else if (collision.name.Contains("Star"))
         {
+            StageClearSound.PlayOneShot(StageClearSound.clip);
             ObtainedStar++;
+            if (ObtainedStar == NeedStar)
+            {
+                StarItemSound.PlayOneShot(StarItemSound.clip);
+            }
             Destroy(collision.gameObject);
         }
         else if (collision.name.Contains("JumpItem"))
         {
+            JumpItemSound.PlayOneShot(JumpItemSound.clip);
             ObtainedJumpItem++;
             Destroy(collision.gameObject);
         }
         else if (collision.name.Contains("Heart"))
         {
+            HeartItemSound.PlayOneShot(HeartItemSound.clip);
             ObtainedHeart++;
             RemainingHeart++;
             Destroy(collision.gameObject);
@@ -93,13 +129,27 @@ public class GameSystem : MonoBehaviour
                 MeshRenderer PastCheckPointMesh = PastCheckPoint.GetComponent<MeshRenderer>();
                 PastCheckPointMesh.material.color = Color.gray;
             }
-            PastCheckPoint = collision.gameObject;
+            if (PastCheckPoint != collision.gameObject)
+            {
+                CheckPointSound.PlayOneShot(CheckPointSound.clip);
+                PastCheckPoint = collision.gameObject;
+            }
             MeshRenderer TmpMesh = collision.GetComponent<MeshRenderer>();
             TmpMesh.material.color = Color.green;
         }
         else if (collision.name.Contains("Magma"))
         {
             RemainingHeart = 0;
+        }
+        else if (collision.name.Contains("Damage"))
+        {
+            RemainingHeart--;
+            DamageSound.PlayOneShot(DamageSound.clip);
+            if (RemainingHeart == 1)
+            {
+                HeartOnly1Sound.PlayOneShot(HeartOnly1Sound.clip);
+            }
+            Destroy(collision.gameObject);
         }
     }
 }
